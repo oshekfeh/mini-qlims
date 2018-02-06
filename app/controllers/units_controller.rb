20175bf47506081a8905e5b43b2c98d294603aab
Omar Shekfeh <oshekfeh@adlqatar.qa>
2018-02-06 14:56:10 +0300

    Test unit actions

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,2 +1,37 @@
 class UnitsController < ApplicationController
+  before_action :set_unit, only: [:show, :update]
+
+  # GET /todos
+  def index
+    @units = Unit.all
+    json_response(@units)
+  end
+
+  # POST /todos
+  def create
+    @unit = Unit.create!(unit_params)
+    json_response(@unit, :created)
+  end
+
+  # GET /todos/:id
+  def show
+    json_response(@unit)
+  end
+
+  # PUT /todos/:id
+  def update
+    @unit.update(unit_params)
+    head :no_content
+  end
+
+  private
+
+  def unit_params
+    # whitelist params
+    params.permit(:name)
+  end
+
+  def set_unit
+    @unit = Unit.find(params[:id])
+  end
 end
